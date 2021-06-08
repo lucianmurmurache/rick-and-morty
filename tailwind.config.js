9ab57fd3e45ed284b02cbee2ed7c100d2271007c
LucianMurmurache <48526455+lucianmurmurache@users.noreply.github.com>
2021-06-08 14:05:11 +0200

    1st commit - boilerplate

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      screens: {
+        sm: '320px',
+        md: '640px',
+        lg: '1280px',
+        xl: '1920px',
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
