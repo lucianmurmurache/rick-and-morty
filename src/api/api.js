eb8aa253980a2e71614542a4c9ca72130de10c62
LucianMurmurache <48526455+lucianmurmurache@users.noreply.github.com>
2021-06-09 23:28:24 +0200

    Created API file
    It contains the characters endpoint, location and episodes

---
+++ src/api/api.js
@@ -0,0 +1,20 @@
+const BASE_URL = 'https://rickandmortyapi.com/api/';
+
+const request = async (url) => {
+  const response = await fetch(`${BASE_URL}${url}`);
+
+  try {
+    if (!response.ok) {
+      console.log(response.statusText);
+    }
+    const result = await response.json();
+
+    return result;
+  } catch {
+    throw new Error();
+  }
+};
+
+export const getCharacters = () => request('character');
+export const getLocations = () => request('location');
+export const getEpisodes = () => request('episode');
