9d29928501525c0ed065a77c0c45922ae67a8217
LucianMurmurache <48526455+lucianmurmurache@users.noreply.github.com>
2021-06-13 12:27:19 +0200

    Updated home background and minor component changes
    Aligned Footer items
    Removed Rounded table and dark borders
    Removed shadow from Navbar

--- src/components/Locations.js
+++ src/components/Locations.js
@@ -23,8 +23,8 @@ const Locations = () => {
     <div className='flex flex-col dark:bg-gray-900'>
       <div className='-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8'>
         <div className='py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8'>
-          <div className='shadow overflow-hidden border-b border-gray-200 sm:rounded-lg'>
-            <table className='min-w-full divide-y divide-gray-200'>
+          <div className='shadow overflow-hidden'>
+            <table className='min-w-full divide-y divide-gray-200 dark:divide-gray-500'>
               <thead className='bg-gray-50 dark:bg-gray-900'>
                 <tr>
                   <th className='px-6 py-3 text-left font-medium text-gray-500 dark:text-white uppercase tracking-wider'>
@@ -38,7 +38,7 @@ const Locations = () => {
                   </th>
                 </tr>
               </thead>
-              <tbody className='bg-white divide-y divide-gray-200'>
+              <tbody className='bg-white divide-y divide-gray-200 dark:bg-gray-900 dark:divide-gray-500'>
                 {locations.map((location) => (
                   <tr key={location.id}>
                     <td className='px-6 py-4 whitespace-nowrap dark:bg-gray-900'>
